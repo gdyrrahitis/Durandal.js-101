032870f196a4e67dd7baf2d544e62ebd94fd3f83
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2017-02-08 00:37:10 +0000

    Fixed property binding

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,28 @@ let config: RequireConfig = {
         "transitions": "../node_modules/durandal/js/transitions",
         "knockout": "../node_modules/knockout/build/output/knockout-latest",
         "jquery": "../node_modules/jquery/dist/jquery",
-        "bootstrap": "../node_modules/bootstrap/dist/js/bootstrap"
+        "bootstrap": "../node_modules/bootstrap/dist/js/bootstrap",
+        "pad": "../node_modules/pad/lib/index",
+        "i18n-iso-countries": "../node_modules/i18n-iso-countries/index",
+        "json": "../node_modules/requirejs-plugins/src/json",
+        "codes": "../node_modules/i18n-iso-countries/codes.json"
     },
     shim: {
         "bootstrap": {
             deps: ["jquery"]
+        },
+        "i18n-iso-countries": {
+            deps: ["pad"]
         }
     },
     urlArgs: `=${new Date().getTime()}`
 };
 
+define("pad", [], function (pad) {
+    return pad;
+});
 requirejs.config(config);
-require(['jquery', 'bootstrap'], () => {
+require(['jquery', 'bootstrap', 'i18n-iso-countries'], () => {
     require(["./bootstrapper"], (b) => {
         var instance = new b();
         instance.init();
--- src/viewmodels/propertyBinding.ts
+++ src/viewmodels/propertyBinding.ts
@@ -1,5 +1,6 @@
 import * as app from "durandal/app";
 import * as ko from "knockout";
+import { getName } from "i18n-iso-countries";
 
 class PropertyBinding {
     private flagpediaUrlTemplate: string = "http://flagpedia.net/data/flags/normal/$P0.png";
@@ -7,9 +8,19 @@ class PropertyBinding {
 
     public title = "Property binding";
     public code = ko.observable();
-    public flagUrl = ko.observable(); 
+    public flagUrl = ko.observable();
 
-    public showCountryFlag () {
+    public get name() {
+        let code: any = this.code();
+        if (!code || (code && code.length < 2)) {
+            return "";
+        }
+
+        let country = getName(<any>code, "en");
+        return country;
+    }
+
+    public showCountryFlag() {
         this.hasClicked(true);
 
         let url = this.flagpediaUrlTemplate.replace("$P0", this.code().toString());
